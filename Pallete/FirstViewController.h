abb6f03bf303fb0731d161a91560b087f9bfddf5
Sohailkh <sohailkhanifar@gmail.com>
2015-02-05 23:14:15 -0800

    Initial Commit

---
+++ Pallete/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Pallete
+//
+//  Created by Sohail Khanifar on 2/5/15.
+//  Copyright (c) 2015 Sohail Khanifar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
